e60528dfcbc0b19b28f8b3482edd8560484080fd
shuvamkumarsahoo-hue <shuvamkumarsahoo@gmail.com>
2026-04-25 23:28:05 +0530

    Shuvam added problem-4

---
+++ level_1/SamAverage.java
@@ -0,0 +1,11 @@
+public class SamAverage {
+    public static void main(String[] args) {
+        double maths = 94;
+        double physics = 95;
+        double chemistry = 96;
+
+        double average = (maths + physics + chemistry) / 3;
+
+        System.out.println("Sam's average mark in PCM is ___ " + average + " ___");
+    }
+}
