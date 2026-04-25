8d3d381a84bb1c7cf8841b1b2de27798813a9b91
shuvamkumarsahoo-hue <shuvamkumarsahoo@gmail.com>
2026-04-25 22:25:20 +0530

    initial commit

---
+++ level_1/DistanceConverter.java
@@ -0,0 +1,10 @@
+public class DistanceConverter {
+	public static void main(String[] args) {
+		double distance = 10.8;
+		
+		double milesDistance = 10.8 * 1.8;
+		
+		System.out.println("Distance into miles for 10.8 kms is " + milesDistance);
+		
+	}
+}
